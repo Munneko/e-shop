f165876925c33695af142ad68c28d0688141b441
Diana <munneko13@gmail.com>
2025-07-01 23:00:51 +0300

    sign up/sign in UI

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,43 +1,53 @@
-'use client'
+"use client";
 
-import { useState } from 'react'
-import { Button } from '@/components/ui/button'
-import { ChevronLeft } from 'lucide-react'
-import { useRouter } from 'next/navigation'
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { ChevronLeft } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function AuthPage() {
-  const [isSignUp, setIsSignUp] = useState(true)
-  const router = useRouter()
+  const [isSignUp, setIsSignUp] = useState(true);
+  const router = useRouter();
 
   const toggleForm = () => {
-    setIsSignUp(!isSignUp)
-  }
+    setIsSignUp(!isSignUp);
+  };
 
   return (
-    <div className='flex min-h-screen mt-7'>
-      <div className='w-full max-w-3xl mx-auto flex flex-col lg:flex-row'>
-
-        <div className='lg:w-1/4 flex items-start'>
+    <div className="flex min-h-screen mt-7">
+      <div className="w-full max-w-3xl mx-auto flex flex-col lg:flex-row">
+        <div className="lg:w-1/4 flex items-start">
           <div
-            className='mb-8 lg:mb-12 cursor-pointer'
-            onClick={() => router.push('/')}
+            className="mb-8 lg:mb-12 cursor-pointer"
+            onClick={() => router.push("/")}
           >
-            <ChevronLeft className='w-6 h-6 text-gray-500 rounded-full border-2' />
+            <ChevronLeft className="w-6 h-6 text-gray-500 rounded-full border-2" />
           </div>
         </div>
 
-        <div className='lg:w-3/4'>
-          <h2 className='text-3xl sm:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent pb-3'>
-            {isSignUp ? 'Sign Up' : 'Sign In'}
+        <div className="lg:w-3/4">
+          <h2 className="text-3xl sm:text-4xl lg:text-5xl font-bold mb-4 sm:mb-6 bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent pb-3">
+            {isSignUp ? "Sign Up" : "Sign In"}
           </h2>
-          <p className='text-base sm:text-lg lg:text-xl text-gray-400 mb-6 sm:mb-8'>
+          <p className="text-base sm:text-lg lg:text-xl text-gray-400 mb-6 sm:mb-8">
             {isSignUp
-              ? 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum gravida ullamcorper diam, ac ultrices diam auctor eu. Integer viverra nulla vitae lectus volutpat porta. Aliquam erat volutpat. '
-              : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'}
+              ? "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum gravida ullamcorper diam, ac ultrices diam auctor eu. Integer viverra nulla vitae lectus volutpat porta. Aliquam erat volutpat. "
+              : "Lorem ipsum dolor sit amet, consectetur adipiscing elit."}
           </p>
+          <div className="mt-4 sm:mt-5 flex items-center justify-center">
+            <p className="text-base sm:text-lg lg:text-xl text-gray-600">
+              {isSignUp ? "Already a member?" : "Don't have an account?"}
+            </p>
+            <Button
+              variant="link"
+              className="text-lg sm:text-xl lg:text-2xl text-gray-500 cursor-pointer"
+              onClick={toggleForm}
+            >
+              {isSignUp ? "Sign in" : "Sign up"}
+            </Button>
+          </div>
         </div>
-
       </div>
     </div>
-  )
+  );
 }
